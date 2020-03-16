de1ac6c06caf837c62cfd4346218966133b2dce2
Adrian Ochmann <adrian.ochmann@outlook.com>
2020-03-16 15:07:14 +0100

    feat: added logic for downloading secure file

--- preDownloadMultipleSecureFiles.ts
+++ preDownloadMultipleSecureFiles.ts
@@ -3,7 +3,31 @@ import secureFilesCommon = require('securefiles-common/securefiles-common');
 import tl = require('azure-pipelines-task-lib/task');
 
 async function run() {
-  
+  try {
+    tl.setResourcePath(path.join(__dirname, 'task.json'));
+
+    let retryCount: number = parseInt(tl.getInput('retryCount') as string);
+
+    if (isNaN(retryCount) || retryCount < 0) {
+      retryCount = 5;
+    }
+
+    let secureFilePaths: string[] = new Array<string>();
+
+    const secureFiles: string[] = tl.getDelimitedInput('secureFiles', '\n', true);
+    const secureFileHelpers: secureFilesCommon.SecureFileHelpers = new secureFilesCommon.SecureFileHelpers(retryCount);
+
+    secureFiles.forEach(async secureFile => {
+      let secureFilePath: string = await secureFileHelpers.downloadSecureFile(secureFile);
+
+      if (tl.exist(secureFilePath)) {
+        secureFilePaths.push(secureFilePath);
+        tl.setVariable('secureFilePaths', secureFilePaths.join(','));
+      }
+    });
+  } catch (err) {
+    tl.setResult(tl.TaskResult.Failed, err);
+  }
 }
 
 run();
