dc73af75344f35c8f4efd2a240a3cd48ec306cb5
Adrian Ochmann <adrian.ochmann@outlook.com>
2020-03-16 15:06:12 +0100

    feat: added initial project setup

---
+++ preDownloadMultipleSecureFiles.ts
@@ -0,0 +1,9 @@
+import path = require('path');
+import secureFilesCommon = require('securefiles-common/securefiles-common');
+import tl = require('azure-pipelines-task-lib/task');
+
+async function run() {
+  
+}
+
+run();
