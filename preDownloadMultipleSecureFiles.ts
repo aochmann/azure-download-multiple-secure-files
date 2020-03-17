5380e24c39c1744b7cb807106ccc2886208bfac6
Adrian Ochmann <adrian.ochmann@outlook.com>
2020-03-17 14:54:05 +0100

    feat: refactor code

--- preDownloadMultipleSecureFiles.ts
+++ preDownloadMultipleSecureFiles.ts
@@ -1,6 +1,8 @@
-import path = require('path');
+const path = require('path');
+const tl = require('azure-pipelines-task-lib/task');
+
+// @ts-ignore
 import secureFilesCommon = require('securefiles-common/securefiles-common');
-import tl = require('azure-pipelines-task-lib/task');
 
 async function run() {
   try {
@@ -11,23 +13,25 @@ async function run() {
     if (isNaN(retryCount) || retryCount < 0) {
       retryCount = 5;
     }
+    
+    const secureFiles: string[] = getSecureFiles();
+    const secureFilesPath: string[] = await downloadSecureFiles(secureFiles, retryCount);
+    const secureFilePathsOutput: string = secureFilesPath.filter(secureFilePath => tl.exist(secureFilePath)).join(',');
 
-    let secureFilePaths: string[] = new Array<string>();
-
-    const secureFiles: string[] = tl.getDelimitedInput('secureFiles', '\n', true);
-    const secureFileHelpers: secureFilesCommon.SecureFileHelpers = new secureFilesCommon.SecureFileHelpers(retryCount);
-
-    secureFiles.forEach(async secureFile => {
-      let secureFilePath: string = await secureFileHelpers.downloadSecureFile(secureFile);
-
-      if (tl.exist(secureFilePath)) {
-        secureFilePaths.push(secureFilePath);
-        tl.setVariable('secureFilePaths', secureFilePaths.join(','));
-      }
-    });
+    tl.setVariable('secureFilePaths', secureFilePathsOutput);
   } catch (err) {
     tl.setResult(tl.TaskResult.Failed, err);
   }
+}
+
+function getSecureFiles(): Array<string> {
+  return tl.getDelimitedInput('secureFiles', '\n', true);
+}
+
+async function downloadSecureFiles(secureFiles: Array<string>, retryCount: number = 5): Promise<Array<string>> {
+  const secureFileHelpers: secureFilesCommon.SecureFileHelpers = new secureFilesCommon.SecureFileHelpers(retryCount);
+  const results: Promise<string>[] = secureFiles.map(async secureFile => await secureFileHelpers.downloadSecureFile(secureFile) as string)
+  return await Promise.all<string>(results);
 }
 
 await run();
